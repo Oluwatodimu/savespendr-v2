af8b42763daec9064a9c7e84ff75f387a3e971bf
todimuisewon <todimuisewon@gmail.com>
2024-12-28 00:10:13 +0100

    added wallet microservice

--- wallet-service/src/main/java/com/savespendr/backend/walletservice/service/impl/WalletServiceImpl.java
+++ wallet-service/src/main/java/com/savespendr/backend/walletservice/service/impl/WalletServiceImpl.java
@@ -1,12 +1,16 @@
 package com.savespendr.backend.walletservice.service.impl;
 
+import com.savespendr.backend.walletservice.dto.Currency;
 import com.savespendr.backend.walletservice.entity.Wallet;
-import com.savespendr.backend.walletservice.entity.enums.Symbol;
 import com.savespendr.backend.walletservice.repository.WalletRepository;
 import com.savespendr.backend.walletservice.service.WalletService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 @Service
@@ -22,11 +26,20 @@ public class WalletServiceImpl implements WalletService {
     @Override
     public void createUserWallets(UUID userId) {
         // get the available currencies
-        // create wallet based on NGN and USD
-        // save
+        List<Currency> currenciesSupported = new ArrayList<>();
+        currenciesSupported.stream()
+                .map(currency -> createWallet(userId, currency.getId()))
+                .filter(Objects::nonNull)
+                .forEach(walletRepository::save);
     }
 
-    private Wallet createWallet(UUID userId, Symbol symbol) {
-        return null;
+    private Wallet createWallet(UUID userId, UUID currencyId) {
+        Wallet wallet = new Wallet();
+        wallet.setAvailableBalance(BigDecimal.ZERO);
+        wallet.setReservedBalance(BigDecimal.ZERO);
+        wallet.setLocked(false);
+        wallet.setCurrencyId(currencyId);
+        wallet.setUserId(userId);
+        return wallet;
     }
 }

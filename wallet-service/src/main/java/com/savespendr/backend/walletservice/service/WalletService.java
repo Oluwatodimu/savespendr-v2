f4516a90e7045dc90953e1d75f8a6a5f4fe88fa9
todimuisewon <todimuisewon@gmail.com>
2024-12-29 23:48:37 +0100

    added wallet microservice

--- wallet-service/src/main/java/com/savespendr/backend/walletservice/service/WalletService.java
+++ wallet-service/src/main/java/com/savespendr/backend/walletservice/service/WalletService.java
@@ -1,7 +1,5 @@
 package com.savespendr.backend.walletservice.service;
 
-import com.savespendr.backend.walletservice.dto.DepositRequest;
-
 import java.util.UUID;
 
 public interface WalletService {
